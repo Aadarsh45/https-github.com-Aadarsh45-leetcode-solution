c47052e4ad8bed8a6cfe6d7b440073d10ac7dd2a
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2023-05-22 22:14:04 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (31.54%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -7,7 +7,7 @@ class Solution {
         int m = INT_MIN;
         dp[0] = 0, dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            if (i % 2) dp[i] = dp[i / 2] + dp[i / 2 + 1];
+            if (i % 2 == 1) dp[i] = dp[i / 2] + dp[i / 2 + 1];
             else dp[i] = dp[i / 2];
             m = max(dp[i], m);
         }
