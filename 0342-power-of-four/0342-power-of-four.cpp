aae1bffe5de6076fae31bbd4a93dc575421f0539
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2023-10-23 14:56:59 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (13.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-         if(n <= 0) return false;
-         return ceil(log2(n) / log2(4)) == floor(log2(n) / log2(4));
+         if(n==0){
+            return false;
+        }
+        else if(n==1){
+            return true;
+        }
+        return n%4==0 && isPowerOfFour(n/4); 
         
     }
 };
