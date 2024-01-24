7bcc91e40b83eb415c423cb45d8dca4a15769ee0
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-01-25 00:45:02 +0530

    Time: 244 ms (91.42%), Space: 177.7 MB (98.93%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int isrequired(vector<int> &map){
+        int oddCount = 0;
+        for(auto i : map){
+            if(i%2==1){
+                oddCount++;
+            }
+        }
+        if(oddCount>1) return 0;
+        return 1;
+    }
+    int recur(TreeNode* curr, vector<int> &map){
+        if(!(curr->left) && !(curr->right)){
+            map[curr->val]++;
+            int ans = isrequired(map);
+            map[curr->val]--;
+            return ans;
+        }
+        map[curr->val]++;
+        int ans = 0;
+        if(curr->left){
+            ans += recur(curr->left, map);
+        }
+        if(curr->right){
+            ans += recur(curr->right, map);
+        }
+        map[curr->val]--;
+        return ans;
+    }
+
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<int> map(10,0);
+        return recur(root,map);
+    }
+};
