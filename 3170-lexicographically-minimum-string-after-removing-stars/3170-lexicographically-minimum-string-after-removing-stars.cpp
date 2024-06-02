539a791f9b52bb7e0ec8248a38c8864384dc4ca9
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-06-02 15:09:14 +0530

    Time: 203 ms (66.67%), Space: 26 MB (66.67%) - LeetHub

---
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    struct info{
+        char c;
+        int ind;
+    };
+    struct comp{
+        bool operator()(const info& e1, const info& e2)const{
+            if (e1.c==e2.c){
+                return e1.ind < e2.ind;
+            }
+            return e1.c > e2.c;
+        }
+    };
+public:
+    string clearStars(string s) {
+        
+        int n = s.length();
+        string ans;
+        info temp;
+        priority_queue<info,vector<info>,comp> pq;
+        vector<char>v(n,'A');
+        
+        for(int i =0;i<n;i++){
+            if(s[i] == '*'){
+                   temp = pq.top();
+                    pq.pop();
+                    s[temp.ind] = '!';
+                   continue;
+             }
+            temp.c = s[i];
+            temp.ind = i;
+            pq.push(temp);
+        }
+        if(pq.empty()) return "";
+        
+        
+        string res = "";
+        for (char c: s){
+            if (c >= 'a') { 
+                res += c; 
+            }
+        }
+        return res;
+        
+        
+    }
+};
+
