17b175e61c6072780f38d0ce6e1d3385a6ae45f9
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-07-14 12:30:23 +0530

    Time: 412 ms (100.00%), Space: 282.7 MB (11.11%) - LeetHub

---
+++ 3219-minimum-cost-for-cutting-cake-ii/3219-minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& hor, vector<int>& ver) {
+        vector<pair<int,char>> vec;
+
+        for(int i =0;i<hor.size();i++){
+            vec.push_back({hor[i],'H'});
+            
+        }
+        for(int j =0;j<ver.size();j++){
+            vec.push_back({ver[j],'V'});
+        }
+        sort(vec.begin(),vec.end(),greater());
+        
+        long long totalcost = 0;
+        int v = 1;
+        int h = 1;
+        
+        for(auto it: vec){
+            int val = it.first;
+            char c = it.second;
+            
+            
+            if(c == 'H'){
+                totalcost+=v*val;
+                h++;
+            }
+            else{
+                totalcost += h*val;
+                v++;
+            }
+            // cout<<val<<" "<<c<<" "<<totalcost<<endl;
+        }
+        return totalcost;
+    }
+};
