756c320d8381f4856c9c0e2a3ca12e9ca78efe02
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-08-19 13:44:03 +0530

    Time: 15 ms (36.48%), Space: 87.4 MB (19.17%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int help(int n,int len,int paste,vector<int>&dp){
+    int help(int n,int len,int paste,vector<vector<int>>&dp){
         if(len > n) return 1000;
         if(len == n) return 0;
+        
+        if(dp[len][paste]!= 0) return dp[len][paste];
 
         int cpy = 2+help(n,len*2,len,dp);
         int p = 1+help(n,len+paste,paste,dp);
 
-        return min(cpy,p);
+        return dp[len][paste] =  min(cpy,p);
         
         
         
@@ -15,7 +17,7 @@ public:
     int minSteps(int n) {
         if(n == 1)return 0;
 
-        vector<int> dp(n+1,n+1);
+        vector<vector<int>>dp(n + 1, vector<int>(n / 2 + 1, 0));
         return 1+ help(n,1,1,dp);
         
     }
