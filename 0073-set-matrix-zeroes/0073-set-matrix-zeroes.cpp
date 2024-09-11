4a9159fd0ef27f328fa3b156ceae083c4210ee15
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-09-11 14:42:38 +0530

    Time: 8 ms (77.01%), Space: 17 MB (25.64%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
-    void row(int i,vector<vector<bool>>& vec,vector<vector<int>>& matrix){
-        
-        for(int k = 0;k<vec[0].size();k++){
-            if(matrix[i][k] != 0){
-                vec[i][k] = false;
-            }
-        }
-    }
-    
-    void col(int j,vector<vector<bool>>& vec,vector<vector<int>>& matrix){
-        for(int k = 0;k<matrix.size();k++){
-            if(matrix[k][j] != 0){
-                vec[k][j] = false;
-            }
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<bool> >vec( m, vector<bool> (n, true)); 
-        
-        for(int i = 0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    row(i,vec,matrix);
-                    col(j,vec,matrix);
-                }
-            }
-        }
-        
-        for(int i = 0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(vec[i][j] == false){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-        
+        int n = matrix.size();
+        int m = matrix[0].size();
 
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(matrix[i][j] == 0){
+                    row[i] = -1;
+                    col[j] = -1;
+                }
+            }
+        }
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(row[i] == -1 || col [j] == -1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
         
     }
 };
