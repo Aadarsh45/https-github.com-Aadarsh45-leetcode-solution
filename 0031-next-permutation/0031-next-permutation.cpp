5dab95cab8042ae7a101ee5c2bfabd155297e5fc
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-09-11 16:02:40 +0530

    Time: 0 ms (100%), Space: 14.9 MB (53.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,19 +1,29 @@
+
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-      int n = nums.size(), index = -1;
-        for(int i=n-2; i>=0; i--){
-            if(nums[i] < nums[i+1]){
-                index = i;
-                break;
-            }
+    void nextPermutation(std::vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+        
+        // Find the first decreasing element
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
         }
-        for(int i=n-1; i>=index && index != -1; i--){
-            if(nums[i] > nums[index]){
-                swap(nums[i], nums[index]);
-                break;
+        
+        if (i >= 0) {
+            int j = n - 1;
+            // Find the element just larger than nums[i]
+            while (nums[j] <= nums[i]) {
+                j--;
             }
+            std::swap(nums[i], nums[j]);
+        } else {
+            // If no such element is found, reverse the entire array
+            std::reverse(nums.begin(), nums.end());
+            return;
         }
-        reverse(nums.begin() + index + 1, nums.end());   
+        
+        // Reverse the elements from i+1 to the end of the array
+        std::reverse(nums.begin() + i + 1, nums.end());
     }
 };
