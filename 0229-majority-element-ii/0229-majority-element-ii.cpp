99218f594fa43c04a59be8571c85309a7a7eb9f8
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-09-15 12:45:17 +0530

    Time: 14 ms (16.37%), Space: 18.7 MB (54.04%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int count=n/3;
-        vector<pair<int,int>> vp;
-        vector<int> v;
-        map<int,int> mp;
-        for(int i=0;i<n;i++)
-        {
-            mp[nums[i]]++;
-        }    
-        for(auto x:mp)
-        {
-            vp.push_back(make_pair(x.second,x.first));
+        int maj1 = 0;
+        int maj2 = 0;
+        int cnt1 = 0,cnt2 = 0;
+
+        for(auto it: nums){
+            if(it == maj1){
+                cnt1++;
+            }
+            else if(it == maj2){
+                cnt2++;
+            }
+            else if(cnt1 == 0){
+                maj1 = it;
+                cnt1++;
+            }
+            else if(cnt2 == 0){
+                maj2 = it;
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
-        for(int i=0;i<vp.size();i++)
-        {
-            if(vp[i].first>count)
-            v.push_back(vp[i].second);
+        cnt1 = 0;cnt2 = 0;
+        for(auto it:nums){
+            if(it == maj1){
+                cnt1++;
+            }
+            else if(it == maj2){
+                cnt2++;
+            }
         }
-        return v;
+        vector<int>res;
+
+        if(cnt1> nums.size()/3){
+            res.push_back(maj1);
+        }
+        if(cnt2> nums.size()/3){
+            res.push_back(maj2);
+        }
+
+        return res;
+
+        
     }
 };
