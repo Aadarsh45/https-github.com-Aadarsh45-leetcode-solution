c423fc1c22bf6bf6e2c7cbf40b848ef96b98465d
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-09-15 15:24:08 +0530

    Time: 30 ms (38.91%), Space: 16.2 MB (74.89%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
         int n = nums.size();
-        vector<vector<int>>ans;
-        
-        for(int i = 0;i<n;i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            for(int j = i+1;j<n;j++){
-                if(j>i+1 && nums[j] == nums[j-1] ) continue;
-                int k = j+1;
-                int l = n-1;
-                while(k<l){
-                   long long sum = nums[i];
-                sum += nums[j];
-                sum += nums[k];
-                sum += nums[l];
-                    if(sum == target){
-                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                        k++;
-                        l--;
-                        while(k<l && nums[k] == nums[k-1]) k++;
-                        while(k<l && nums[l] == nums[l+1]) l--;
-                        
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
+
+        for(int i = 0;i<n-3;i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for(int j = i+1;j<n-2;j++){
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; 
+
+                int lo = j+1,hgh = n-1;
+
+                while(lo < hgh){
+                    long long sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[lo] + (long long)nums[hgh];
+                    if (sum == target) {
+                        res.push_back({nums[i], nums[j], nums[lo], nums[hgh]});
+                        while (lo < hgh && nums[lo] == nums[lo + 1]) lo++; // Skip duplicates for the third number
+                        while (lo < hgh && nums[hgh] == nums[hgh - 1]) hgh--; // Skip duplicates for the fourth number
+                        lo++;
+                        hgh--;
                     }
-                    else if( sum < target){
-                       k++; 
-                    }
-                    else{
-                        l--;
-                    }
+                    else if(sum < target) lo++;
+                    else hgh--;
+                    
                 }
-            }
+            
         }
-        return ans;
+     
         
+    }
+       return res;
     }
 };
