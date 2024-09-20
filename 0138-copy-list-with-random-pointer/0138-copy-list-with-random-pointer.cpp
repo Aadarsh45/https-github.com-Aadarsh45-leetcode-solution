dfcf3802372509ef66b4d9465f113e5b2cab84a2
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-09-20 18:14:45 +0530

    Time: 7 ms (53.97%), Space: 14.3 MB (98.72%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,66 +1,53 @@
-/*
 // Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
+// class Node {
+// public:
+//     int val;
+//     Node* next;
+//     Node* random;
     
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
+//     Node(int _val) {
+//         val = _val;
+//         next = nullptr;
+//         random = nullptr;
+//     }
+// };
 
 class Solution {
 public:
-    void insertNode(Node* head){
-        Node* temp = head;
-        while(temp!= nullptr){
-             Node* new_node = new Node(temp->val);
-             new_node->next = temp->next;
-             temp->next = new_node;
-             temp = temp->next->next;
-            
-             
-        }
-    }
-    void getPointers(Node* head){
-        
-        Node* temp = head;
-        while(temp!=nullptr){
-            Node* copy = temp->next;
-           
-            if(temp->random!=NULL){
-                copy->random = temp->random->next;
-            }
-            
-            temp = temp->next->next;
-        }
-      
-    }
-    Node* getCopy(Node* head){
-        Node* dummy = new Node(-1);
-        Node* res = dummy;
-        
-        Node* temp = head;
-        while(temp!=nullptr){
-            res->next = temp->next;
-            res = res->next;
-            
-            temp->next = temp->next->next;
-            temp = temp->next;
-        }
-        return dummy->next;
-    }
-    
     Node* copyRandomList(Node* head) {
-        insertNode(head);
-        getPointers(head);
-        return getCopy(head);
-        
-        
+        if (head == nullptr) return nullptr;
+
+        // Step 1: Create new nodes and insert them after each original node
+        Node* curr = head;
+        while (curr != nullptr) {
+            Node* newNode = new Node(curr->val);
+            newNode->next = curr->next;
+            curr->next = newNode;
+            curr = newNode->next;
+        }
+
+        // Step 2: Assign random pointers for the new nodes
+        curr = head;
+        while (curr != nullptr) {
+            if (curr->random != nullptr) {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+
+        // Step 3: Separate the new list from the original list
+        curr = head;
+        Node* newHead = head->next;
+        Node* copy = newHead;
+        while (curr != nullptr) {
+            curr->next = curr->next->next;
+            if (copy->next != nullptr) {
+                copy->next = copy->next->next;
+            }
+            curr = curr->next;
+            copy = copy->next;
+        }
+
+        return newHead;
     }
 };
