3ed51474e9b653766447d2065ffa64ed67de4691
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-09-20 23:29:28 +0530

    Time: 21 ms (7.95%), Space: 25.9 MB (22.99%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int>pre(n,0);
-        vector<int>suff(n,0);
-        pre[0] = height[0];
-        suff[n-1] = height[n-1];
+    int trap(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 0) return 0;
 
-        for(int i = 1;i<n;i++){
-            pre[i] = max(pre[i-1],height[i]);
+        vector<int> pre(n);
+        pre[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = max(pre[i - 1], arr[i]);
         }
-        for(int i = n-2;i>=0;i--){
-            suff[i] = max(suff[i+1],height[i]);
+
+        vector<int> suff(n);
+        suff[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suff[i] = max(suff[i + 1], arr[i]);
         }
-        
-       int water = 0;
-       
-       for(int i = 0;i<n;i++){
-        int mini = min(pre[i],suff[i]);
-        water += mini - height[i];
-       }
-       return water;
-    
-       
-        
+
+        int water = 0;
+        for (int i = 0; i < n; i++) {
+            int mini = min(pre[i], suff[i]);
+            water += mini - arr[i];
+        }
+        return water;
     }
 };
