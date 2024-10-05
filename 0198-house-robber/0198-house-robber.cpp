b83d980be85478fff0c6c18b173044d68f52f608
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-10-06 02:35:22 +0530

    Time: 4 ms (32.04%), Space: 9.5 MB (46.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,54 +1,35 @@
 class Solution {
 public:
-
-//     int solve(vector<int>&nums,int n,vector<int>&dp){
-        
-
-//         if(n<0) return 0;
-
-//         if(n==0) return  nums[0];    // base condition
-
-//         if(dp[n]!=-1){
-//             return dp[n];
-//         }
-
-//         int pick = nums[n] + solve(nums,n-2,dp);
-//         int notpick = 0 + solve(nums,n-1,dp);
-
-//         return dp[n] = max(pick,notpick);
-
-
-//     }
     int rob(vector<int>& nums) {
-//         int n = nums.size()-1;
-        
-        
-//         vector<int>dp(n+1,-1);
-        
-//         return solve(nums,n,dp); int prev2 = 0;
-        int prev1 = nums[0];
-        int prev2 = 0;
-        
-        
-        for(int i = 1; i<nums.size();i++){
-            
-            int take  = nums[i];
-              if(i > 1) {
-                  take+=prev2;
-                  
-              }
-            
-            int not_take = prev1;
-            
-            int curr = max(take,not_take);
-            prev2=prev1;
-            prev1=curr;
-           
-            
+
+        int n = nums.size();
+        if(n == 1)return nums[0];
+        vector<int>dp(n,0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i = 2;i<n;i++){
+            int pick = nums[i]+dp[i-2];
+            int not_pick  = dp[i-1];
+            dp[i] = max(pick,not_pick);
         }
-        return prev1;
-      
-        
+        return dp[n-1];
         
     }
 };
+
+// int maxLootRec(vector<int> &hval, int n) {
+    
+//     // If no houses are left, return 0.
+//     if (n <= 0)  return 0;
+  
+//       // If only 1 house is left, rob it. 
+//     if (n == 1)  return hval[0];
+
+//     // Two Choices: Rob the nth house and do not rob the nth house 
+//     int pick = hval[n - 1] + maxLootRec(hval, n - 2);
+//     int notPick = maxLootRec(hval, n - 1);
+
+//     // Return the max of two choices
+//     return max(pick, notPick);
+// }
