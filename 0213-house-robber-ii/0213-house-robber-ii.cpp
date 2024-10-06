a858eceeae26be6562a84e0a9286081fa132c0b5
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-10-06 14:54:14 +0530

    Time: 4 ms (32.85%), Space: 9.7 MB (56.43%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>&nums){   
-       
-        int prev2 = 0;
-        int prev1 = nums[0];
-        
-        
-        for(int i = 1; i<nums.size();i++){
-            
-            int take  = nums[i];
-              if(i > 1) {
-                  take+=prev2;
-                  
-              }
-            
-            int not_take = prev1;
-            
-            int curr = max(take,not_take);
-            prev2=prev1;
-            prev1=curr;
-           
-            
-        }
-        return prev1;
-      
-      
-    
-    }
-    int rob(vector<int>& nums) {
+    int help(vector<int>&nums){
         int n = nums.size();
-        if(n == 1){
-            return nums[0];
+        if(n == 1)return nums[0];
+        vector<int>dp(n,0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i = 2;i<n;i++){
+            dp[i] = max(dp[i-1],nums[i]+dp[i-2]);
+            cout<<i<<" "<<dp[i]<<endl;
         }
-         vector<int> temp1(begin(nums), end(nums)-1);
-        vector<int> temp2(begin(nums)+1, end(nums));
+        return dp[n-1];
+    }
+    int rob(vector<int>& input) {
+        int n = input.size();
+        if(n == 1)return input[0];
+
+        vector<int> first(input.begin(), input.end() - 1);  // From 0 to n-2
+        vector<int> second(input.begin() + 1, input.end());
+
+        return max(help(first),help(second));
         
-        
-        return max(solve(temp1),solve(temp2));
     }
 };
