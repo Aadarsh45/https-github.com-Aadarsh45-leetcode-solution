0585712afd61c159ace458662d6a368fc1ce6e62
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-10-12 17:53:57 +0530

    Time: 46 ms (33.97%), Space: 46.9 MB (43.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int help(int i, vector<int>& coins, int amount, vector<vector<int>>& dp) {
-        if (amount == 0) return 0;
-        if (i == 0) {
-            if (amount % coins[0] == 0) return amount / coins[0];
-            else return INT_MAX;
-        }
-        
-        if (dp[i][amount] != -1) return dp[i][amount];
-        
-        int t = INT_MAX;
-        if (coins[i] <= amount) {
-            int res = help(i, coins, amount - coins[i], dp);
-            if (res != INT_MAX) {
-                t = 1 + res;
-            }
-        }
-        int nt = help(i - 1, coins, amount, dp);
-        return dp[i][amount] = min(t, nt);
-    }
-
-    
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        int res = 1e9+7;
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        res = min(res,help(n-1,coins,amount,dp));
+        vector<vector<int>> dp(n, vector<int>(amount + 1, INT_MAX));
         
-        return res == 1e9+7 ? -1 : static_cast<int>(res);
+        // Initialize the first row of the DP table
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0) {
+                dp[0][i] = i / coins[0];
+            } 
+        }
         
+        // Fill the DP table
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int t = INT_MAX;
+                if (coins[i] <= j) {
+                    int res = dp[i][j - coins[i]];
+                    if (res != INT_MAX) {
+                        t = 1 + res;
+                    }
+                }
+                int nt = dp[i - 1][j];
+                dp[i][j] = min(t, nt);
+            }
+        }
+        
+        int res = dp[n - 1][amount];
+        return res == INT_MAX ? -1 : res;
     }
 };
