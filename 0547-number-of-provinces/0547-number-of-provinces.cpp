a7eb63662a1267a860e1acaec3aa94c0d2adc8db
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-11-11 16:10:34 +0530

    Time: 3 ms (47.91%), Space: 19.2 MB (53.31%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,40 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-
-      void dfs(int s, int n, vector<vector<int>>& isConnected, vector<bool>&visited){
-        visited[s] = true;
-        
-        
-        vector<int>adj;
-        for(int i=0; i<n; i++ ){
-            int x =  isConnected[s][i];  // x = connection
-            if(x == 1)
-                adj.push_back(i);
-        }
-        
-        for(auto x: adj){
-            if(!visited[x]){
-                dfs(x, n, isConnected, visited);
+    void dfs(int i, vector<vector<int>>& adj, vector<bool>& vis) {
+        vis[i] = true;
+        for (auto it : adj[i]) {
+            if (!vis[it]) {
+                dfs(it, adj, vis);
             }
         }
-        
-      }    
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        
-        int n=isConnected.size();
-         vector<bool> visited(n,0);
-         int ans=0;
+    }
 
-         for(int i=0;i<isConnected.size();i++){
-             if(!visited[i]){
-                 ans++;
-                 dfs(i,n,isConnected,visited);
-             }
-         }
-       
-        return ans;
+    int findCircleNum(vector<vector<int>>& nums) {
+        int n = nums.size();
+
+        // Create an adjacency list
+        vector<vector<int>> adj(n); 
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (nums[i][j] == 1 && i != j) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        // Perform DFS to count connected components
+        int cnt = 0;
+        vector<bool> vis(n, false);
+        for (int i = 0; i < n; ++i) {
+            if (!vis[i]) {
+                cnt++;
+                dfs(i, adj, vis);
+            }
+        }
+
+        return cnt;
     }
 };
