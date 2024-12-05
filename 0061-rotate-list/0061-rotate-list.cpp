f63c24d873e2542fc7ad3a95b1a357837d8db4a3
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-12-06 03:04:00 +0530

    Time: 0 ms (100%), Space: 16.5 MB (70.61%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,36 +11,24 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head ==nullptr || head->next == nullptr || k==0)return head;
-        int size = 0;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            size++;
-            temp = temp->next;
-        }
-        if(k%size == 0){
-            return head;
-        }
-        size = size - k%size;
-        ListNode* st = head;
-        ListNode* prev = st;
-       
-        while(size--){
-            prev = st; 
-            st = st->next;
-
-        }
-        prev->next = nullptr;
-        ListNode* res = st;
-        while(st!=nullptr){
-            if(st->next == nullptr){
-                st->next = head;
-                break;
-            }
-            st = st->next;
-        }
+          if(head == NULL||head->next == NULL||k == 0) return head;
+    //calculating length
+    ListNode* temp = head;
+    int length = 1;
+    while(temp->next != NULL) {
+        ++length;
+        temp = temp->next;
+    }
+    //link last node to first node
+    temp->next = head;
+    k = k%length; //when k is more than length of list
+    int end = length-k; //to get end of the list
+    while(end--) temp = temp->next;
+    //breaking last node link and pointing to NULL
+    head = temp->next;
+    temp->next = NULL;
         
-        return res;
+    return head;
         
     }
 };
