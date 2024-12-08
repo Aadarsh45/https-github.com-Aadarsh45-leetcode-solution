ade9469d1fc5bfe981bb994cb82b2d4e6770769d
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-12-08 16:50:48 +0530

    Time: 68 ms (30.89%), Space: 34.8 MB (24.73%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
+
+        vector<vector<int>>cnt;
         sort(nums.begin(),nums.end());
         int n = nums.size();
-
-        int i = 0;
-        while(i<n-2){
-            if (i > 0 && nums[i] == nums[i - 1]) {i++; continue; }
-             int j = i+1;
-             int k = n-1;
-           
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum == 0){
-                   vector<int> temp = {nums[i], nums[j], nums[k]};
-                    ans.push_back(temp);
-                     j++;
-                     k--;
-                //skip the duplicates:
-                while (j < k && nums[j] == nums[j - 1]) j++;
-                while (j < k && nums[k] == nums[k + 1]) k--;
-                }
-                else if(sum > 0){
+        for(int i = 0;i< n-2;i++){
+            if( i >= 1 && nums[i] == nums[i-1])continue;
+            int sum = nums[i];
+            int j = i+1;
+            int k = n-1;
+            while(j < k){
+                 int temp =nums[j]+nums[k];
+                 
+                if(sum+temp == 0){
+                    vector<int>temp;
+                    temp.push_back(nums[i]);temp.push_back(nums[j]);temp.push_back(nums[k]);
+                    cnt.push_back(temp);
+                    j++;
                     k--;
+                   
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+
+                }
+                else if(sum+temp > 0){
+                    k--;
+
                 }
                 else{
                     j++;
                 }
             }
-            i++;
-
         }
-        return ans;
+
+        return cnt;
         
     }
 };
+              
